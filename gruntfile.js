e4e9b35842de4f1d4a2daf51be25cc34876f41a5
Alexander Fedotov <alexander.g.fedotov@kaspersky.com>
2017-05-22 21:18:14 +0300

    Added bootstrap and minification

---
+++ gruntfile.js
@@ -0,0 +1,15 @@
+module.exports =  function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: ['js/main.js','js/plugins.js'],
+                dest: 'js/main.min.js'
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['uglify'])
+};
